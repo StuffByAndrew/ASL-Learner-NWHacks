2646aef31ae58c2d99d62fb75fcaa91f9d055223
Harvir Dhaliwal <harvir.dhaliwal@hotmail.com>
2022-01-16 03:35:16 -0800

    removed code

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -33,71 +33,71 @@ import {
 export default function DashboardApp() {
 
   
-  let model, labelContainer, maxPredictions, webcam;
+  // let model, labelContainer, maxPredictions, webcam;
 
-  useEffect(() => {
-    const script1 = document.createElement('script');
-    const script2 = document.createElement('script');
+  // useEffect(() => {
+  //   const script1 = document.createElement('script');
+  //   const script2 = document.createElement('script');
   
-    script1.src = "https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js";
-    script2.src = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js";
+  //   script1.src = "https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js";
+  //   script2.src = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js";
   
-    script1.async = true;
-    script2.async = true;
+  //   script1.async = true;
+  //   script2.async = true;
   
-    document.body.appendChild(script1);
-    document.body.appendChild(script2);
+  //   document.body.appendChild(script1);
+  //   document.body.appendChild(script2);
   
-    return () => {
-      document.body.removeChild(script1);
-      document.body.removeChild(script2);
-    }
-  }, []);
+  //   return () => {
+  //     document.body.removeChild(script1);
+  //     document.body.removeChild(script2);
+  //   }
+  // }, []);
 
       // Load the image model and setup the webcam
-      async function init() {
+      // async function init() {
 
         // load the model and metadata
         // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
         // or files from your local hard drive
         // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-        const model = tf.loadLayersModel('https://teachablemachine.withgoogle.com/models/em1KDmjQF/model.json').then( mod => {
-          console.log(mod);
-        });
+        // const model = tf.loadLayersModel('https://teachablemachine.withgoogle.com/models/em1KDmjQF/model.json').then( mod => {
+        //   console.log(mod);
+        // });
 
         // maxPredictions = await model.getTotalClasses();
 
         // Convenience function to setup a webcam
-        const flip = true; // whether to flip the webcam
-        webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-        await webcam.setup(); // request access to the webcam
-        await webcam.play();
-        window.requestAnimationFrame(loop);
+        // const flip = true; // whether to flip the webcam
+        // webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+        // await webcam.setup(); // request access to the webcam
+        // await webcam.play();
+        // window.requestAnimationFrame(loop);
 
         // append elements to the DOM
-        document.getElementById("webcam-container").appendChild(webcam.canvas);
+        // document.getElementById("webcam-container").appendChild(webcam.canvas);
         // labelContainer = document.getElementById("label-container");
         // for (let i = 0; i < maxPredictions; i++) { // and class labels
         //     labelContainer.appendChild(document.createElement("div"));
         // }
     }
 
-    async function loop() {
-        webcam.update(); // update the webcam frame
-        await predict();
-        window.requestAnimationFrame(loop);
-    }
+    // async function loop() {
+    //     webcam.update(); // update the webcam frame
+    //     await predict();
+    //     window.requestAnimationFrame(loop);
+    // }
 
-    // run the webcam image through the image model
-    async function predict() {
-        // predict can take in an image, video or canvas html element
-        const prediction = await model.predict(webcam.canvas);
-        for (let i = 0; i < maxPredictions; i++) {
-            const classPrediction =
-                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-            labelContainer.childNodes[i].innerHTML = classPrediction;
-        }
-    }
+    // // run the webcam image through the image model
+    // async function predict() {
+    //     // predict can take in an image, video or canvas html element
+    //     const prediction = await model.predict(webcam.canvas);
+    //     for (let i = 0; i < maxPredictions; i++) {
+    //         const classPrediction =
+    //             prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    //         labelContainer.childNodes[i].innerHTML = classPrediction;
+    //     }
+    // }
 
 
 
@@ -173,10 +173,10 @@ export default function DashboardApp() {
           <Grid item xs={4}>
             <Card>
               <Box>
-                <div>Teachable Machine Image Model</div>
+                {/* <div>Teachable Machine Image Model</div>
                 <button type="button" onclick={init()}>Start</button>
                 <div id="webcam-container"></div>
-                <div id="label-container"></div>
+                <div id="label-container"></div> */}
               </Box>
             </Card>
           </Grid>
